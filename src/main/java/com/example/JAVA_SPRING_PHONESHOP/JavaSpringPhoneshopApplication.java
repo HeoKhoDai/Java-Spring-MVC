8759b7b5f76f665a168a971dc49a73ef5c17e6f5
HeoKhoDai <baokhanh20802@gmail.com>
2025-02-14 20:42:08 +0700

    setup spring-security commit

--- src/main/java/com/example/JAVA_SPRING_PHONESHOP/JavaSpringPhoneshopApplication.java
+++ src/main/java/com/example/JAVA_SPRING_PHONESHOP/JavaSpringPhoneshopApplication.java
@@ -3,7 +3,8 @@ package com.example.JAVA_SPRING_PHONESHOP;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class JavaSpringPhoneshopApplication {
 
 	public static void main(String[] args) {
