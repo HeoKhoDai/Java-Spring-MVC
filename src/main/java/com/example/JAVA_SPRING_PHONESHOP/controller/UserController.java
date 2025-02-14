3665ecc58193a840f90f272a4c39a12b406e0e61
HeoKhoDai <baokhanh20802@gmail.com>
2025-02-14 21:07:25 +0700

    setup mvc commit

---
+++ src/main/java/com/example/JAVA_SPRING_PHONESHOP/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.example.JAVA_SPRING_PHONESHOP.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.JAVA_SPRING_PHONESHOP.service.UserService;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+
+    public String getHomePage() {
+        String test = userService.handleHelloWorld();
+        return "index.html";
+    }
+}
---
+++ src/main/java/com/example/JAVA_SPRING_PHONESHOP/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.JAVA_SPRING_PHONESHOP.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public String handleHelloWorld() {
+        return "Hello from service";
+    }
+}
