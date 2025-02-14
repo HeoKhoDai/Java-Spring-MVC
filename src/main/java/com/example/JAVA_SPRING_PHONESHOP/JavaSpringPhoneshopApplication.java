f20fe8adfd9b10d2ac47a33b9c1615ae21232e81
HeoKhoDai <baokhanh20802@gmail.com>
2025-02-14 17:44:56 +0700

    init project

---
+++ src/main/java/com/example/JAVA_SPRING_PHONESHOP/JavaSpringPhoneshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.JAVA_SPRING_PHONESHOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringPhoneshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringPhoneshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JAVA_SPRING_PHONESHOP/JavaSpringPhoneshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JAVA_SPRING_PHONESHOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringPhoneshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
