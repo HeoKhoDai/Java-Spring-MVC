e08b74f64e08e407122098e592788d3ffc793b4b
HeoKhoDai <baokhanh20802@gmail.com>
2025-02-16 21:02:37 +0700

    send data commit

--- src/main/java/com/example/JAVA_SPRING_PHONESHOP/controller/UserController.java
+++ src/main/java/com/example/JAVA_SPRING_PHONESHOP/controller/UserController.java
@@ -2,8 +2,11 @@ package com.example.JAVA_SPRING_PHONESHOP.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.JAVA_SPRING_PHONESHOP.domain.User;
 import com.example.JAVA_SPRING_PHONESHOP.service.UserService;
 
 @Controller
@@ -24,6 +27,13 @@ public class UserController {
 
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
         return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User user) {
+        System.out.println("run here" + user);
+        return "hello";
     }
 }
